992c1f53aca34eee732790a83956c80377036f96
Marcin Wojtach <marcin.vojtach@gmail.com>
2024-11-21 10:12:45 +0100

    initial configuration

---
+++ .playground/file.ts
@@ -0,0 +1,5 @@
+class TestFile {
+    constructor(protected readonly version: string) { }
+}
+
+const testFile: TestFile = new TestFile('1');
---
+++ lua/core/keymaps.lua
@@ -0,0 +1,77 @@
+-- set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- disable space default behaviour
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+local opts = { noremap = true, silent = true }
+
+-- save file without formating
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- quit file
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- delete single char without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
+
+-- vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- find and center
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- resize with arrows
+vim.keymap.set('n', '<Up>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<Down>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Left>', ':vertical resize +2<CR>', opts)
+
+-- buffers
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<leader>bx', ':bdelete<CR>', opts)
+vim.keymap.set('n', '<leader>bn', ':enew<CR>', opts)
+
+-- window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts)      -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts)      -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts)     -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts)   -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts)     --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts)     --  go to previous tab
+
+-- toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- press jk fast to exit insert mode
+vim.keymap.set('i', 'jk', '<ESC>', opts)
+vim.keymap.set('i', 'kj', '<ESC>', opts)
+
+-- stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- move text up and down
+vim.keymap.set('v', '<A-j>', ':m .+1<CR>==', opts)
+vim.keymap.set('v', '<A-k>', ':m .-2<CR>==', opts)
+
+-- keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- replace word under cursor
+vim.keymap.set('n', '<leader>j', '*``cgn', opts)
+
